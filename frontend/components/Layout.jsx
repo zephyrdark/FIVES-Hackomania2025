4659d3dd09228c5f7e8094be200b680954332239
jiekai1 <hojiekai1@gmail.com>
2025-02-15 20:27:46 +0800

    frontend - homepage

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -3,9 +3,9 @@ import MobileNavigation from "./NavBar";
 
 const Layout = ({ children }) => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen bg-white text-black">
             {/* Content Section */}
-            <main className="flex-grow">{children}</main>
+            <main className="flex-grow bg-white">{children}</main>
 
             {/* Fixed Bottom Navigation */}
             <MobileNavigation />
--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,22 +1,36 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+
+import { Search } from "lucide-react";
+import Header from "../components/Header"; // Import the Header component
 
 export default function Home() {
-    const [number, setNumber] = useState(10)
-    const [name, setName] = useState("")
-    useEffect(()=>{
-        setName((prev) => prev += 'abc')
-    }, [number])
-    let exampleArray = { test: 'abc' }
-    useNavigate('/recipe')
     return (
-        <div>
-            Home {exampleArray.test}
-            Number is : {number}
+        <div className="px-3">
+            <div className="min-h-screen bg-gray-100">
+                <Header pageName={"Explore Recipes"} >
+                </Header>
+                <div className="mt-10 flex items-center gap-3">
+                    {/* Search Bar */}
+                    <div className="flex items-center bg-gray-200 px-3 py-2 rounded-full w-full">
+                        <Search size={18} className="text-gray-500" />
+                        <input
+                            type="text"
+                            placeholder="Search Recipe"
+                            className="ml-2 bg-transparent outline-none w-full text-gray-700 placeholder-gray-500"
+                        />
+                    </div>
 
-            Name: {name}
-            <btn onClick={() => { setNumber((prev) => prev + 1) }}>Add one</btn>
+                    {/* SMARTEATS Button */}
+                    <btn className="bg-blue-500 px-4 py-2 rounded-partial text-white font-medium">
+                        SMARTEATS
+                    </btn>
+
+                </div>
+                <div className="pt-2 px-4">
+                    <p className="text-2xl font-bold">Top Recipes</p>
+                </div>
+
+            </div>
+
         </div>
-
-    )
+    );
 }
